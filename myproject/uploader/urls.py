dbf35aee4253593c2da564cacfedbff731e7a685
Anton Masyuk <pantera_box@mail.ru>
2023-09-28 22:30:20 +0500

    for review

--- myproject/uploader/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from uploader import views
-
-app_name = 'uploader'
-
-urlpatterns = [
-    path('', views.upload_video, name='upload_video'),
-]
--- myproject/video/mixins.py
+++
@@ -1,7 +0,0 @@
-class UpdateMixin(object):
-    def update(self, **kwargs):
-        if self._state.adding:
-            raise self.DoesNotExist
-        for field, value in kwargs.items():
-            setattr(self, field, value)
-        self.save(update_fields=kwargs.keys())

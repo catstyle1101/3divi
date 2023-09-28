b0587972c410960464f2e4eac166aba3c4017e9d
Anton Masyuk <pantera_box@mail.ru>
2023-09-28 21:23:16 +0500

    work finished

--- myproject/myproject/celery.py
+++ myproject/myproject/celery.py
@@ -4,4 +4,5 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 app = Celery('myproject')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.broker_connection_retry_on_startup = True
 app.autodiscover_tasks()
